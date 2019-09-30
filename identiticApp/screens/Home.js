bedff341d3f4b8edfd9aad73ae007921b1c0ac9d
Joaquin Polonuer <jtpolonuer@gmail.com>
2019-09-30 14:22:34 -0300

    minor upgrades

--- identiticApp/screens/Home.js
+++ identiticApp/screens/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import { ScrollView, SafeAreaView, Text } from "react-native";
 import { Header } from "react-native-elements";
 import styled from "styled-components";
-import CardBuscarClases from "../components/CardBuscarClases";
-import Card2 from "../components/Card2";
+import CardBuscarClases from "./components/CardBuscarClases";
+import Card2 from "./components/Card2";
 
-import Subject from "../components/Subjects";
+import Subject from "./components/Subjects";
 import LinearGradient from "react-native-linear-gradient";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Home() {
+export default function AApp() {
   return (
     <Container>
       <Header
@@ -37,7 +37,7 @@ export default function Home() {
 
         <ScrollView
           horizontal={true}
-          style={{ paddingBottom: 10 }}
+          style={{ bottom: 100 }}
           showsHorizontalScrollIndicator={false}
         >
           <CardBuscarClases
@@ -48,7 +48,7 @@ export default function Home() {
             subtitle="5 of 12 sections"
           />
           <ScrollView
-            horizontal={false}
+            style={{ width: 250 }}
             contentContainerStyle={{ alignItems: "center" }}
           >
             <Card2
@@ -67,14 +67,20 @@ export default function Home() {
             />
           </ScrollView>
         </ScrollView>
-        <Subtitle>Hoy</Subtitle>
+        <Subtitle>Materias de Hoy</Subtitle>
 
         <ScrollView
+          style={{ bottom: 90 }}
           horizontal={false}
           contentContainerStyle={{ alignItems: "center" }}
         >
           {subjects.map((subject, index) => (
-            <Subject key={index} text={subject.text} image={subject.image} />
+            <Subject
+              key={index}
+              text={subject.text}
+              image={subject.image}
+              mark={subject.mark}
+            />
           ))}
         </ScrollView>
       </ScrollView>
@@ -85,40 +91,42 @@ export default function Home() {
 const subjects = [
   {
     image: require("./assets/ico-matematica.png"),
-    text: "Matematica"
+    text: "Matematica",
+    mark: "7:45 - 9:05"
   },
   {
     image: require("./assets/ico-lengua.png"),
-    text: "Lengua"
+    text: "Lengua",
+    mark: "9:20 - 10:40"
   },
   {
     image: require("./assets/ico-historia.png"),
-    text: "Historia"
+    text: "Historia",
+    mark: "10:55 - 12:15"
   },
   {
     image: require("./assets/ico-quimica.png"),
-    text: "Quimica"
+    text: "Quimica",
+    mark: "13:05 - 14:30"
   }
 ];
 
 const Avatar = styled.Image`
-  width: 80px;
-  height: 80px;
+  width: 60px;
+  height: 60px;
   background-color: black;
-  border-radius: 40px;
-  margin-left: 20px;
-  position: absolute;
-  top: 0;
-  left: 0;
+  border-radius: 30px;
+  right: 20%;
+  top: 15%;
+  position: relative;
 `;
 
 const Subtitle = styled.Text`
   color: #b8bece;
-  font-weight: 600;
-  font-size: 17px;
-  margin-left: 20px;
-  margin-bottom: 0px;
-  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 18px;
+  bottom: 15%;
+  left: 5%;
 `;
 
 const Container = styled.View`
@@ -126,17 +134,21 @@ const Container = styled.View`
   background-color: whitesmoke;
 `;
 const Title = styled.Text`
-  font-size: 20px;
-  color: #b8bece;
-  font-weight: 500;
-  margin-left: 30px;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 51px;
+  color: #5f5f5f;
+  left: 8%;
+  bottom: 25%;
 `;
 
 const Name = styled.Text`
-  font-size: 24px;
+  font-size: 18px;
   color: #3c4560;
   font-weight: bold;
-  margin-left: 30px;
+  line-height: 51px;
+  left: 8%;
+  bottom: 44%;
 `;
 const Bar = styled.Text`
   font-size: 25px;
@@ -146,7 +158,6 @@ const Bar = styled.Text`
 
 const TitleBar = styled.View`
   width: 100%;
-  margin-top: 13px;
-  margin-bottom: 30px;
+  margin-bottom: 50px;
   padding-left: 80px;
 `;
